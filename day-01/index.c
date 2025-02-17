840cbb3a310a89ae2a1a9717c38e7434b731a6e5
BharathKShetty <bks29525@gmail.com>
2025-02-17 12:27:47 +0530

    Update index.c

--- day-01/index.c
+++ day-01/index.c
@@ -1,67 +1,73 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<string.h>
-void reademployee(int *p_id, char* name, float* p_salary,int* p_attendance){
-//read employee
-   printf("Enter Id:");
-   scanf("%d",p_id);
-   
-   printf("Enter your name:");
-   scanf("%s",name);
-   
-   printf("Enter the salary:");
-   scanf("%f",p_salary);
-   
-   printf("Enter number  of days worked:");
-   scanf("%d",p_attendance);
+void readEmployee(int* p_id, char* name, float* p_salary, int* p_attendance) {
+    printf("Enter id:");
+    scanf("%d", p_id);
+
+    printf("Enter name:");
+    scanf("%s", name);
+
+    printf("Enter salary:");
+    scanf("%f", p_salary);
+    
+    printf("Enter number of days worked:");
+    scanf("%d", p_attendance);
 }
-float calculateBonous(float salary, int attendance)
-{
+
+float calculateBonus(float salary, int attendance) {
     float bonus = 0.0f;
-     if(attendence>25){
-       bonus=(salary/100.0f)*5.0f;
-   }else if (attendence<10){
-    //   bonus=(-1.0f*((bonus/100.0f)*50.0f));
-        bonus=-(salary/2);
-   }
-   return bonus;
+    if(attendance > 25) {
+        bonus = (salary / 100.0f) * 5.0f;
+    } else if(attendance < 10) {
+        bonus = (-1.0f * ((salary / 100.0f) * 50.0f));
+    }
+    return bonus;
 }
-void categorizeemployee(char* category, int attendance){
-      if(attendence >25){
-       strcpy(category,"excellent");
-   }else if(attendence<15){
-       strcpy(category,"Need improvement");
-   }
+
+void categorizeEmployee(char* category, int attendance) {
+    if(attendance > 25) {
+        strcpy(category, "Excellent");
+    } else if(attendance < 15) {
+        strcpy(category, "Need Improvement");
+    }
+}
+
+void printPayroll(int id, char* name, float salary, 
+                 float bonus, float gross_salary,
+                 char* category) {
+    printf("Payroll Details:\n");
+    printf("\tId: %d\n", id);
+    printf("\tName: %s\n", name);
+    printf("\tBasic Salary: %.2f\n", salary);
+    if(bonus > 0) {
+        printf("\tBonus: %.2f\n", bonus);
+    }
+    printf("\tFinal Salary: %.2f\n", gross_salary);
+    printf("\tEmployee Performance Category: %s\n", category);
 }
 int main() {
-    // Write C code here
-   int id;
-   char name[255];
-   float salary;
-   int attendence;
-   
-   //read employee
-   reademployee(&id,name,&salary,&attendence);
-   
-   //process payroll]
-   
-   float gross_salary = 0.0f;
-   char category[50]="Good";
-   
-   //calculation bonus
-   
-  
-   //final salary
-   gross_salary=(salary+bonus);
-   //categorize employee
- 
-   //display payroll details
-   printf("Id: %d\n",id);
-   printf("Name: %s\n",name);
-   printf("Basic salary: %.2f\n",salary);
-   if(bonus>0){
-       printf("Bonus:%.2f\n",bonus);
-   }
-   printf("FInal salary:%.2f\n",gross_salary);
-   printf("Employee performace category:%s\n",category);
+    int id;
+    char name[255];
+    float salary;
+    int attendance;
+
+    //read employee     - 1
+    readEmployee(&id, name, &salary, &attendance);
+
+    //process payroll
+    float bonus = 0.0f;
+    float gross_salary = 0.0f;
+    char category[50] = "Good";
+    
+    //  calculate bonus         -2
+    bonus = calculateBonus(salary, attendance);
+    //  final salary 
+    gross_salary = (salary + bonus);
+    //  categorize employee      -3
+    categorizeEmployee(category, attendance);
+
+    //display payroll details      -4
+    printPayroll(id, name, salary, 
+                 bonus, gross_salary, category);
     return 0;
-}
+} 
