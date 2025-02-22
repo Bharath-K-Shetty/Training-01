ac58da7073a721f78b125b78afc3e3263925288d
BharathKShetty <bks29525@gmail.com>
2025-02-22 12:02:30 +0530

    Create sortInsertion.c

---
+++ day-06/sortInsertion.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int searchLinear(float salaries[], int size, float searchSalary);
+int searchBinary(float salaries[], int size, float searchSalary);
+int main() {
+    float salaries[] = { 12.0f, 13.0f, 15.0f,20.0f, 30.0f};
+    int salariesCount = 5;
+    float searchSalary = 20.0f;
+
+    int index = searchLinear(salaries, salariesCount, searchSalary);
+    printf("%.2f found at index %d\n", searchSalary, index);
+    int mid=searchBinary(salaries, salariesCount, searchSalary);
+    printf("%.2f found at index %d\n", searchSalary, mid);
+    return 0;
+}
+
+int searchLinear(float salaries[], int size, float searchSalary) {
+    for (int i = 0; i < size; i++) {
+        if (salaries[i] == searchSalary) {
+            return i;
+        }
+    }
+    return -1;
+}
+int searchBinary(float salaries[], int size, float searchSalary) {
+    int left = 0, right = size - 1;
+
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        if (salaries[mid] == searchSalary) {
+            return mid;
+        } else if (searchSalary < salaries[mid]) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return -1;
+}
